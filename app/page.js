34ce1b0ab118617b8fbcc838091cece1ee8ce879
Bustamante Barrett-Greening <146181717+busta4@users.noreply.github.com>
2024-09-13 12:45:03 +0100

    fixed error with container class

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#181C14]">
       <Navbar />
-      <div className="mt-24 container, mx-auto px-12 py-4">
+      <div className="mt-24 container mx-auto px-12 py-4">
         <section><HeroSection /></section>
         <section id="about" className="scroll-mt-24"><AboutSection/></section>
         <section id="projects" className="scroll-mt-24"><ProjectSection/></section>
