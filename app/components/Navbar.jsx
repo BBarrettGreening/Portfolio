90e3964aad1cebcf63740f412729ed739192e9c2
Bustamante Barrett-Greening <146181717+BBarrettGreening@users.noreply.github.com>
2025-07-14 14:32:35 +0100

    changed navbar colour to match background

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -24,8 +24,7 @@ const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     return (
-        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-            {/* Increased padding for a bit more breathing room */}
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#181C14] bg-opacity-90">
             <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
                 <Link href="/" className="text-2xl md:text-5xl text-white font-semibold">
                     Bustamante Barrett-Greening
@@ -42,8 +41,6 @@ const Navbar = () => {
                     )}
                 </div>
                 <div className="menu hidden md:block md:w-auto" id="navbar">
-                    {/* Removed `mt-10` to align items vertically */}
-                    {/* Added `md:space-x-8` for horizontal spacing on medium screens and up */}
                     <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8">
                         {navLinks.map((link, index) => (
                             <li key={index}>
