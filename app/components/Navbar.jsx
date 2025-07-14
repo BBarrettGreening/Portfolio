136056ce785161c234a04e850ad380b8af9e530c
Bustamante Barrett-Greening <146181717+BBarrettGreening@users.noreply.github.com>
2025-07-14 14:30:44 +0100

    improved navbar styling

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -20,41 +20,42 @@ const navLinks = [
     }
 ]
 
-
-
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
-    return (
-    <nav className="fixed top-0 left-0 right-0 z-5 bg-[#181C14] bg-opacity-90">
-        <div className="flex flex-wrap items-center justify-between mx-auto px-4">
-            <Link href="/" className="text-2xl text-white font-semibold">Bustamante Barrett-Greening</Link>
-            <div className="mobile-menu block md:hidden">
-            {
-                !navbarOpen ? (
-                    <button onClick={ () => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white">
-                    <Bars3Icon className="h-5 w-5" />
-                    </button>
-                ) : (
-                    <button onClick={ () => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white">
-                    <XMarkIcon className="h-5 w-5" />
-                    </button>
-                )
-            }
 
-            </div>
-            <div className="menu hidden md:block md:w-auto" id="navbar">
-                <ul className="flex p-4 md:p-0 md:flex-row md-space-x-8 mt-10">
-                    {navLinks.map((link, index) => (
+    return (
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+            {/* Increased padding for a bit more breathing room */}
+            <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+                <Link href="/" className="text-2xl md:text-5xl text-white font-semibold">
+                    Bustamante Barrett-Greening
+                </Link>
+                <div className="mobile-menu block md:hidden">
+                    {!navbarOpen ? (
+                        <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+                            <Bars3Icon className="h-5 w-5" />
+                        </button>
+                    ) : (
+                        <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+                            <XMarkIcon className="h-5 w-5" />
+                        </button>
+                    )}
+                </div>
+                <div className="menu hidden md:block md:w-auto" id="navbar">
+                    {/* Removed `mt-10` to align items vertically */}
+                    {/* Added `md:space-x-8` for horizontal spacing on medium screens and up */}
+                    <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8">
+                        {navLinks.map((link, index) => (
                             <li key={index}>
                                 <NavLink href={link.path} title={link.title} />
-                            </li> 
+                            </li>
                         ))}
-                </ul>
+                    </ul>
+                </div>
             </div>
-        </div>
-        {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-    </nav>
-    )
-}
+            {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
