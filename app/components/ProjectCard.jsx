225f997be62aa6f27b09f42cc85f45476f010e3e
Bustamante Barrett-Greening <146181717+BBarrettGreening@users.noreply.github.com>
2025-08-18 09:05:24 +0100

    added conditional statement to correctly hide preview URL when not present

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -19,10 +19,12 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
                 <CodeBracketIcon className="h-10 w-10 text-[#adb7be] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute cursor-pointer group-hover/link:text-white" />
             </Link>
             )}
+            {previewUrl && previewUrl !== "/" && (
             <Link href={previewUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#adb7be] hover:border-white group/link">
                 <EyeIcon className="h-10 w-10 text-[#adb7be] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute cursor-pointer group-hover/link:text-white" />
             </Link>
+            )}
         </div>
       </div>
         <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
